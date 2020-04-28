a29e40997001a3a616312c061cbb68d554b0c587
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-28 14:45:35 -0700

    getting data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
-import React from 'react'
-import {Store} from './Store'
+import React from "react";
+import { Store } from "./Store";
 
-
-export default function App():JSX.Element {
-  const {state, dispatch}= React.useContext(Store)
-
+export default function App(): JSX.Element {
+  const { state, dispatch } = React.useContext(Store);
 
   React.useEffect(() => {
-    state.episodes.length === 0 && fetchDataAction()
-  })
+    state.episodes.length === 0 && fetchDataAction();
+  });
 
   const fetchDataAction = async () => {
-    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
-    const data = await fetch(URL)
+    const URL =
+      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
+    const data = await fetch(URL);
     const dataJSON = await data.json();
     return dispatch({
-      type: 'FETCH_DATA',
-      payload: dataJSON._embedded.episodes
-    })
-  }
-  {console.log(state)}
+      type: "FETCH_DATA",
+      payload: dataJSON._embedded.episodes,
+    });
+  };
+
   return (
-  
     <React.Fragment>
       <h1>Rick and Morty</h1>
       <p> Pick your favorite episode!</p>
+      <section>
+        {state.episodes.map((episode: any) => {
+          return (
+            <section key={episode.id}>
+              <img src={episode.image.medium} alt={`Rick and Morty ${episode.name}`}/>
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season} Number: {episode.number}
+              </section>
+            </section>
+          );
+        })}
+      </section>
     </React.Fragment>
-  )
+  );
 }
